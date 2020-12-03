b5521d5f8c64df9f6be4020b1f426eafaaf104c2
gm <gm@stackunderflow.com>
2020-12-03 11:03:39 +0000

    better logging on gpio_shutdown

--- gpio_shutdown.py
+++ gpio_shutdown.py
@@ -20,18 +20,26 @@ GPIO_NUM = int(sys.argv[1])
 SHUTDOWN_TIMER = int(sys.argv[2])
 SHUTDOWN_COMMAND = sys.argv[3]
 
+PIN_STATE = ["low","high"]
+
 gpio.setmode(gpio.BCM)
-print(f"Monitoring GPIO {GPIO_NUM} low for {SHUTDOWN_TIMER}s -> run '{SHUTDOWN_COMMAND}'")
+print(f"Monitoring GPIO {GPIO_NUM} high for {SHUTDOWN_TIMER}s -> run '{SHUTDOWN_COMMAND}'")
 gpio.setup(GPIO_NUM, gpio.IN, pull_up_down=gpio.PUD_UP)
 
+last_state = None
 
 shutdown = SHUTDOWN_TIMER
 while True:
     state = gpio.input(GPIO_NUM)
+    if state != last_state:
+        print(f"pin {GPIO_NUM} state is now {PIN_STATE[state]}")
+    last_state = state
     if state == 0: # pin low
         shutdown = SHUTDOWN_TIMER
     else:
         shutdown -= 1
+        if (shutdown % 10) == 0:
+            print(f"pin {GPIO_NUM} still high, shutdown in {shutdown}s")
     
     if shutdown < 0:
         os.system(SHUTDOWN_COMMAND)
