7d5a249753af07cf176bb0a4000b05fde31446fb
Geordie Millar <gm@stackunderflow.com>
2021-08-08 21:07:21 +1000

    move gps_status icon into backend thread so updates happen when background

--- apps/gps_status/__init__.py
+++ apps/gps_status/__init__.py
@@ -118,6 +118,18 @@ class gps_status(app):
                     report = gpsd.next()
                     self.gps_data[report['class']] = report
                     self.data_updated = True
+
+                    tpv = self.gps_data.get('TPV',{})
+                    skydata = self.gps_data.get('SKY',{}).get('satellites', [])
+                    numsats = len(skydata) # FIXME: de-duplicate this code
+                    numused = len([i for i in skydata if i['used']])
+                    sats_text = f"{numused}/{numsats}"
+
+                    self.status_icon.update(
+                        mode=self.gps_mode(tpv.get('mode','')),
+                        sats=sats_text
+                    )
+                    self.status_icons_updated = True
             except OSError as e:
                 self.gps_data = {'status': f'Connection error: {str(e)}'}
                 self.data_updated = True
@@ -128,6 +140,15 @@ class gps_status(app):
             self.gps_satview.draw(screen, (400, self.display.TOP_BAR_SIZE + 32))
             return True
         return False
+
+    @staticmethod
+    def gps_mode(m):
+        return {
+            0: 'UNK',
+            1: 'NONE',
+            2: '2D',
+            3: '3D'
+        }.get(m,'?')
 
     def update(self, dt):
         if self.data_updated:
@@ -144,13 +165,7 @@ class gps_status(app):
             title = f"{devices.get('driver', '')} {devices.get('subtype','')} at {devices.get('path','')}:{devices.get('bps','')}:8{devices.get('parity','')}{devices.get('stopbits','')}"
             self.ui_element_values['status'].set_text(title)
 
-            gps_mode = {
-                0: 'UNK',
-                1: 'NONE',
-                2: '2D',
-                3: '3D'
-            }.get(tpv.get('mode'),'?')
-            self.ui_element_values['mode_t'].set_text(gps_mode)
+            self.ui_element_values['mode_t'].set_text(self.gps_mode(tpv.get('mode')))
 
             sats_text = ""
             if 'SKY' in self.gps_data:
@@ -173,8 +188,5 @@ class gps_status(app):
                     for lbl in ['PRN', 'el', 'az', 'ss', 'used']:
                         self.satellite_rows[i][lbl].set_text('')
                 self.ui_element_values['sats'].set_text('')
-
-            self.status_icon.update(mode=gps_mode, sats=sats_text)
-            self.status_icons_updated = True
 
         super().update(dt)
